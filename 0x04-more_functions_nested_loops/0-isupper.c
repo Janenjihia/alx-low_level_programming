28693ef93a5a646155f635db96571b9c20568515
Janenjihia <janecyprian199@gmail.com>
2022-09-19 10:48:57 -0700

    correction

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,15 +6,17 @@
  */
 int _isupper(int c)
 {
-	   char uppercase = 'A';
-	        int isupper = 0;
-		      for (; uppercase <= 'Z'; uppercase++)
-			               {
-					                  if (c == uppercase)
-								                 {
-											                  isupper = 1;
-													                   break;
-															                  }
-							           }
-		      return (isupper);
+		char uppercase = 'A';
+			int isupper = 0;
+
+				for (; uppercase <= 'Z'; uppercase++)
+						{
+									if (c == uppercase)
+												{
+																isupper = 1;
+																			break;
+																					}
+										}
+
+					return (isupper);
 }
